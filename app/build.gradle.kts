281be7c598439a3870f04c2a978fd04a44536dd3
NullXoid <echolabsent@gmail.com>
2026-04-28 18:07:59 -0400

    Bump Android debug release to v0.1.46

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,10 +69,10 @@ android {
         versionCode = providers.gradleProperty("APP_VERSION_CODE")
             .orElse(providers.environmentVariable("APP_VERSION_CODE"))
             .map(String::toInt)
-            .getOrElse(45)
+            .getOrElse(46)
         versionName = providers.gradleProperty("APP_VERSION_NAME")
             .orElse(providers.environmentVariable("APP_VERSION_NAME"))
-            .getOrElse("0.1.45")
+            .getOrElse("0.1.46")
     }
 
     buildFeatures {
