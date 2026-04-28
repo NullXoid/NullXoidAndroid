a26fff3ec3b02d2b59858b97b249691091a9b47c
NullXoid <echolabsent@gmail.com>
2026-04-28 17:11:44 -0400

    Align Android debug version with v0.1.41

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,10 +69,10 @@ android {
         versionCode = providers.gradleProperty("APP_VERSION_CODE")
             .orElse(providers.environmentVariable("APP_VERSION_CODE"))
             .map(String::toInt)
-            .getOrElse(14)
+            .getOrElse(41)
         versionName = providers.gradleProperty("APP_VERSION_NAME")
             .orElse(providers.environmentVariable("APP_VERSION_NAME"))
-            .getOrElse("0.1.14")
+            .getOrElse("0.1.41")
     }
 
     buildFeatures {
