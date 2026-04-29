4cfba8d3a13a57f333ad4d8bbda40d6751974ebf
NullXoid <echolabsent@gmail.com>
2026-04-29 08:39:32 -0400

    Bump Android timestamp release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,10 +69,10 @@ android {
         versionCode = providers.gradleProperty("APP_VERSION_CODE")
             .orElse(providers.environmentVariable("APP_VERSION_CODE"))
             .map(String::toInt)
-            .getOrElse(70)
+            .getOrElse(71)
         versionName = providers.gradleProperty("APP_VERSION_NAME")
             .orElse(providers.environmentVariable("APP_VERSION_NAME"))
-            .getOrElse("0.1.70")
+            .getOrElse("0.1.71")
     }
 
     buildFeatures {
